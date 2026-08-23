ac03c9049879c29e8f2b3b8ce081dcaaf05acbdc
Saurabh262004 <saurabhrjadhav26@gmail.com>
2026-08-23 12:36:08 +0530

    why are you running

--- scripts/compile_linux.bash
+++ scripts/compile_linux.bash
@@ -2,20 +2,9 @@
 
 set -e
 
-RUN="${1:-no}"
-NVIDIA="${2:-no}"
-
 BUILD_DIR="example/build/linux"
 
 cmake -B "$BUILD_DIR" -S . -DCMAKE_BUILD_TYPE=Release
 cmake --build "$BUILD_DIR"
 
 OUT="$BUILD_DIR/app"
-
-if [[ "$RUN" == "-r" ]]; then
-	if [[ "$NVIDIA" == "-n" ]]; then
-		__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia "$OUT"
-	else
-		"$OUT"
-	fi
-fi
